4ccab5996d716566a7c9a972e80af22a4a3bdf88
Joel Myers <joeldmyers@gmail.com>
2018-09-01 14:02:45 -0400

    search component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Imported components
+import SearchBar from './components/search_bar';
+
+// youtube API key for searches
 const YOUTUBE_API_KEY = 'AIzaSyB1ACzPBlhVNVlHxFZgPiJHo4Q9riuV9PI';
 
-
+// App
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
-
-reactDOM.render(<App />, document.querySelector('.container'));
+// Render App to DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
